ba240a238606124a2017f65292036b4eb17e7578
Adam Sadlik <72628208+abam-iksde@users.noreply.github.com>
2024-01-26 23:16:06 +0100

    Put `game_scene` into a UI scene (#4)

---
+++ scripts/keep_aspect.gd
@@ -0,0 +1,19 @@
+extends SubViewportContainer
+
+
+@export var aspect_components := Vector2(16.0, 9.0)
+
+
+func on_size_changed():
+	var viewport_size := get_viewport().size as Vector2i
+	var new_size := Vector2(viewport_size.x, viewport_size.x/aspect_components.x * aspect_components.y)
+	if new_size.y <= viewport_size.y:
+		size = new_size
+	else:
+		size = Vector2(viewport_size.y/aspect_components.y * aspect_components.x, viewport_size.y)
+	position = (Vector2(viewport_size) - size)/2.0
+
+
+func _ready():
+	on_size_changed()
+	get_viewport().connect('size_changed', on_size_changed)
