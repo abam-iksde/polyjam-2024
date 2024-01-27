dcbb48938323eaf41def41c60bd71c2be341a902
Adam Sadlik <72628208+abam-iksde@users.noreply.github.com>
2024-01-27 20:48:24 +0100

    Add audio script (#48)
    Co-authored-by: AMVROZIAK <kuba4300@gmail.com>

---
+++ scripts/audio.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+func spawn_sound_effect(bus: String, effect: Resource, pitch_range: Array[int] = [], boost: float = 0.0) -> void:
+	var new_sound_effect := AudioStreamPlayer.new()
+	
+	new_sound_effect.bus = bus
+	new_sound_effect.stream = effect
+	if boost:
+		new_sound_effect.volume_db += boost
+	
+	if pitch_range and len(pitch_range) > 1:
+		new_sound_effect.pitch_scale = randf_range(pitch_range[0], pitch_range[1])    
+	
+	new_sound_effect.finished.connect(new_sound_effect.queue_free)
+	
+	add_child(new_sound_effect)
+	new_sound_effect.play()
