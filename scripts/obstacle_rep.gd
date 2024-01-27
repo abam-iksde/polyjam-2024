88022b9ed035086ac4a73158f14c65dbb6e26a78
Adam Sadlik <72628208+abam-iksde@users.noreply.github.com>
2024-01-27 05:22:39 +0100

    Fix obstacle pixel size

--- scripts/obstacle_rep.gd
+++ scripts/obstacle_rep.gd
@@ -8,6 +8,12 @@ extends Sprite2D
 		generate()
 
 
+@export var texture_scale = 1.0:
+	set(new_scale):
+		texture_scale = new_scale
+		generate()
+
+
 func _ready() -> void:
 	if not Engine.is_editor_hint():
 		return
@@ -16,4 +22,6 @@ func _ready() -> void:
 
 func generate():
 	$collision.size = Vector2(size.x * 256.0, size.y * 256.0)
-	$collision.position = -$collision.size / 2.0
+	scale = Vector2(texture_scale, texture_scale)
+	$collision.scale = (1.0/texture_scale) * Vector2(1.0, 1.0)
+	$collision.position = -$collision.size / 2.0 * $collision.scale
