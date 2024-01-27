9d8792bbbd16722e443af881f803c345c8f0a418
Adam Sadlik <72628208+abam-iksde@users.noreply.github.com>
2024-01-27 01:29:28 +0100

    Shrink bounding boxes

--- scripts/mini_games/soap/soap.gd
+++ scripts/mini_games/soap/soap.gd
@@ -23,7 +23,7 @@ func _physics_process(delta: float) -> void:
 	foot_velocity.x += delta * Input.get_axis('mg_left', 'mg_right') * 1024.0
 	foot_velocity.y += delta * 512.0
 	foot.position += foot_velocity * delta
-	if box_collides(foot, soap):
+	if box_collides(foot.get_node('bounding_box'), soap.get_node('bounding_box')):
 		state = 'won'
 	if foot.position.y >= 0.0:
 		foot.position.y = 0.0
@@ -32,6 +32,6 @@ func _physics_process(delta: float) -> void:
 
 func box_collides(a: Control, b: Control) -> bool:
 	return (
-		a.position.x + a.size.x >= b.position.x and a.position.x < b.position.x + b.size.x and
-		a.position.y + a.size.y >= b.position.y and a.position.y < b.position.y + b.size.y
+		a.global_position.x + a.size.x >= b.global_position.x and a.global_position.x < b.global_position.x + b.size.x and
+		a.global_position.y + a.size.y >= b.global_position.y and a.global_position.y < b.global_position.y + b.size.y
 	)
