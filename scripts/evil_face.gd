9f1f1d34e9b6bb1fcd8b68558a426edeb228cf63
Adam Sadlik <72628208+abam-iksde@users.noreply.github.com>
2024-01-28 15:23:28 +0100

    Boost head's volume

--- scripts/evil_face.gd
+++ scripts/evil_face.gd
@@ -19,7 +19,7 @@ var proximity := 1.0
 
 func _physics_process(delta: float) -> void:
 	if not is_instance_valid(sound_now):
-		sound_now = Audio.spawn_sound_effect("SFX", sounds[randi() % len(sounds)])
+		sound_now = Audio.spawn_sound_effect("SFX", sounds[randi() % len(sounds)], [], 10.0)
 	delta = Motion.get_delta(delta)
 	Motion.head_speed = lerpf(Motion.head_speed, Motion.HEAD_MAX_SPEED, delta * 0.6)
 	proximity = min(proximity - (Motion.head_speed - Motion.movement_speed) * delta, 1.0)
