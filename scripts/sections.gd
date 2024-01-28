1364de6519210385ccab1cc35c48ece81ef59133
Krychowiakj <j.j.krychowiak@gmail.com>
2024-01-28 09:37:51 +0100

    work in progres

--- scripts/sections.gd
+++ scripts/sections.gd
@@ -4,10 +4,15 @@ extends Node
 var SECTIONS := {
 	's0': {
 		section = preload('res://scenes/sections/section0.tscn').instantiate(),
-		next = ['s1'],
+		next = ['s0'],
 	},
 	's1': {
 		section = preload('res://scenes/sections/section1.tscn').instantiate(),
 		next = ['s0'],
 	},
+	's2': {
+		section = preload('res://scenes/sections/section2.tscn').instantiate(),
+		next = ['s1','s0','s2'],
+	},
 }
+
